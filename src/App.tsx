861c172dbbeb1e0023f61599e82021d73838c963
omrajsharma <omraj7413@gmail.com>
2023-09-15 08:31:08 +0530

    feat: header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import EventDetail from './pages/EventDetail'
 import Profile from './pages/Profile'
+import MainLayout from './layout/MainLayout'
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/event/:eventId' element={<EventDetail />} />
-      <Route path='/profile' element={<Profile />} />
+      <Route path='/' element={<MainLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/event/:eventId' element={<EventDetail />} />
+        <Route path='/profile' element={<Profile />} />
+      </Route>
     </Routes>
   )
 }
