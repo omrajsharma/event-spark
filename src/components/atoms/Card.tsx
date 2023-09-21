acc4b6cf6ddf406e152bc7792487629226bf320c
omrajsharma <omraj7413@gmail.com>
2023-09-21 08:45:38 +0530

    component: Card

---
+++ src/components/atoms/Card.tsx
@@ -0,0 +1,14 @@
+type Props = {
+    children: string | JSX.Element | JSX.Element[] 
+    customCss?: string
+}
+
+const Card = ({children, customCss}: Props) => {
+  return (
+    <div className={`inline-block shadow-md rounded-md p-2 ${customCss}`}>
+        {children}
+    </div>
+  )
+}
+
+export default Card
