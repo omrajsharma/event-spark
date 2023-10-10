c8649e707a1597bedcdd290a05a84d68f6c2cc39
omrajsharma <omraj7413@gmail.com>
2023-10-10 08:20:50 +0530

    refactor

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -4,23 +4,23 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import EventSparkLogo from '../../assets/event-spark-logo.png'
 import { Link } from 'react-router-dom'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+// const user = {
+//   name: 'Tom Cook',
+//   email: 'tom@example.com',
+//   imageUrl:
+//     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+// }
 const navigation = [
   { name: 'Tech', href: '#', current: false },
   { name: 'Movie', href: '#', current: false },
   { name: 'Music', href: '#', current: false },
   { name: 'Standup', href: '#', current: false },
 ]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
+// const userNavigation = [
+//   { name: 'Your Profile', href: '#' },
+//   { name: 'Settings', href: '#' },
+//   { name: 'Sign out', href: '#' },
+// ]
 const userNavigationOnLogout = [
   { name: 'Login', href: 'login' },
   { name: 'Signup', href: 'signup' },
